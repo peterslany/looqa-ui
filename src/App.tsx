3df9bc5cb4a435d83ea29e9270a1c18f9915da4e
Peter <peter@interlay.io>
2023-12-23 22:59:49 +0100

    refactor: add placeholder page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="https://user-images.githubusercontent.com/99301796/219900773-d6d02038-e2a0-4334-9f28-c14d40ab6fe7.png" className="logo nostr" alt="Nostr logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        |x|
+        <a href="https://bitcoin.org" target="_blank">
+          <img src="https://logodownload.org/wp-content/uploads/2017/06/bitcoin-logo-1-1.png" className="logo bitcoin" alt="Bitcoin logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>looqa</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          {count} experiences proved
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Coming soon.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
