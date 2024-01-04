4918877455b65e7909a0741b27c7b7df1f6307e5
Peter <peter@interlay.io>
2024-01-04 21:35:11 +0100

    chore: install avvvatars package

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -1,6 +1,18 @@
-
+import "./Feed.css";
 
 const Feed = () => {
-  return <div>Feed page</div>;
+  return (
+    <div>
+      <div className="feed__featured-container">...</div>
+      <main className="feed__main">
+        <div className="feed__main__posts">
+          {/* ADD POSTS COMPONENTS */}
+        </div>
+        <div className="feed__main__sidebar">
+          {/* ADD SIDEBAR */}
+        </div>
+      </main>
+    </div>
+  );
 };
 export { Feed };
